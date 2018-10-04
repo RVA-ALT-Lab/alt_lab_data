ea6381929237ddee496e5d88451b7e47d435f784
Tom Woodward <twwoodward@me.com>
2018-10-04 14:02:25 -0400

    taxonomy for alt lab lead
    may back out of this later but want to slave it to the acf field for this

--- inc/custom-taxonomy.php
+++ inc/custom-taxonomy.php
@@ -76,3 +76,79 @@ function create_dept_taxonomies()
   ));
 }
 
+add_action( 'init', 'create_state_taxonomies', 0 );
+function create_state_taxonomies()
+{
+  // Add new taxonomy, NOT hierarchical (like tags)
+  $labels = array(
+    'name' => _x( 'state', 'taxonomy general name' ),
+    'singular_name' => _x( 'state', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search state' ),
+    'popular_items' => __( 'Popular state' ),
+    'all_items' => __( 'All state' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit state' ),
+    'update_item' => __( 'Update state' ),
+    'add_new_item' => __( 'Add New state' ),
+    'new_item_name' => __( 'New state' ),
+    'add_or_remove_items' => __( 'Add or remove state' ),
+    'choose_from_most_used' => __( 'Choose from the most used state' ),
+    'menu_name' => __( 'state' ),
+  );
+
+//registers taxonomy specific post types - default is just post
+  register_taxonomy('state',array('project'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'state' ),
+    'show_in_rest'          => true,
+    'rest_base'             => 'state',
+    'rest_controller_class' => 'WP_REST_Terms_Controller',
+    'show_in_nav_menus' => false,    
+  ));
+}
+
+add_action( 'init', 'create_lead_taxonomies', 0 );
+function create_lead_taxonomies()
+{
+  // Add new taxonomy, NOT hierarchical (like tags)
+  $labels = array(
+    'name' => _x( 'Leads', 'taxonomy general name' ),
+    'singular_name' => _x( 'lead', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Leads' ),
+    'popular_items' => __( 'Popular Leads' ),
+    'all_items' => __( 'All Leads' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit Leads' ),
+    'update_item' => __( 'Update lead' ),
+    'add_new_item' => __( 'Add New lead' ),
+    'new_item_name' => __( 'New lead' ),
+    'add_or_remove_items' => __( 'Add or remove Leads' ),
+    'choose_from_most_used' => __( 'Choose from the most used Leads' ),
+    'menu_name' => __( 'lead' ),
+  );
+
+//registers taxonomy specific post types - default is just post
+  register_taxonomy('Leads',array('post'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => false,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'lead' ),
+    'show_in_rest'          => true,
+    'rest_base'             => 'lead',
+    'rest_controller_class' => 'WP_REST_Terms_Controller',
+    'show_in_nav_menus' => false,    
+  ));
+}
+
+
+
+
+
--- page-templates/fullwidthpage-projects.php
+++ page-templates/fullwidthpage-projects.php
@@ -22,7 +22,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<main class="site-main" id="main" role="main">
 
 					<?php while ( have_posts() ) : the_post(); ?>
-
+						
 						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
 						<?php
