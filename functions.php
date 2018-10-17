c7683714017abf05be308f74423aff642a260e9d
Tom Woodward <twwoodward@me.com>
2018-10-17 14:36:34 -0400

    this works in rampages but fails on local

--- functions.php
+++ functions.php
@@ -207,7 +207,7 @@ function alt_lab_lead(){
       while( have_rows('alt_lab_specific_information') ): the_row() ;
          $leads = get_sub_field('alt_lab_lead');
           if ($leads){
-            echo '<a href="' . get_author_posts_url( $leads->ID) . '">' . $leads->display_name . '</a>';            
+            echo $leads['display_name'];   //$leads->display_name ************ caution will robinson         
        }
       endwhile;
     endif;
@@ -223,7 +223,7 @@ function alt_author_project ( $post_id ) {
           while( have_rows('alt_lab_specific_information', $post_id) ): the_row() ;
              $leads = get_sub_field('alt_lab_lead', $post_id);
               if ($leads){
-               $author = $leads->ID;            
+               $author = $leads['ID'];     //$leads->ID  ************ caution will robinson   
            }
           endwhile;
     endif;
